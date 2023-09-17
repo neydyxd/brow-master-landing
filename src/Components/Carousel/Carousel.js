e3346dfcd38b673311705f6f9ee9effbd7e6c5fa
Stepan Maksimov <neydyxd@yandex.ru>
2023-09-17 17:52:46 +0300

    Доработка секции слайдера

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,14 +1,6 @@
 import { useState, useEffect, Children, cloneElement } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-const PAGE_WIDTH = 0.32
-const WINDOW_WIDTH = window.screen.width;
-const BLOCK_WIDTH = WINDOW_WIDTH * PAGE_WIDTH
-console.log(WINDOW_WIDTH)
-console.log(BLOCK_WIDTH)
-
-
-
 
 function Carousel({ children }) {
 const [pages, setPages] = useState([]);
@@ -16,6 +8,22 @@ const [offset, setOffset] = useState(0);
 const iconStyle = {
     filter: 'invert(81%) sepia(49%) saturate(859%) hue-rotate(32deg) brightness(104%) contrast(99%)',
 }
+
+
+const WINDOW_WIDTH = window.screen.width;
+
+function calcSliderSize(){
+    if (WINDOW_WIDTH >= 768) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const PAGE_WIDTH = calcSliderSize() ? 0.32 : 1;
+const BLOCK_WIDTH = WINDOW_WIDTH * PAGE_WIDTH
+console.log(PAGE_WIDTH);
+
 
 function handleLeftArrowClick(){
     setOffset((currentOffset) => {
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -4,7 +4,19 @@ function Slider(){
     return(
         <section className='slider'>
             <div className='slider__about'>
-
+                <div className='slider__title-box'>
+                    <h3 className='slider__title_1'>how?</h3>
+                    <h3 className='slider__title_2'>как и где я работаю</h3>
+                    <label className='slider__label'></label>
+                    <h3 className='slider__title_3'>where?</h3>
+                </div>
+                <ul className='slider__list'>
+                    <li className='slider__list-element'>loremlorem loremloremloremlorem</li>
+                    <li className='slider__list-element'>loremlorem loremloremloremlorem</li>
+                    <li className='slider__list-element'>loremlorem loremloremloremlorem</li>
+                    <li className='slider__list-element'>loremlorem loremloremloremlorem</li>
+                    <li className='slider__list-element'>loremlorem loremloremloremlorem</li>
+                </ul>
             </div>
             <Carousel>
                 <div className="slider__item slider__item_1"></div>
